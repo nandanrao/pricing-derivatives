f065a00b850651756aba21f2fbfb770f3ae10196
Nandan Rao <nandanmarkrao@gmail.com>
2017-03-11 15:47:09 +0100

    Boundary Conditions, figures.

--- two/scratch.jl
+++ two/scratch.jl
@@ -3,6 +3,9 @@ using DataFrames
 using Gadfly
 using Base.Test
 
+
+
+
 #######################################
 # Basic Brownian Motion Functions
 #######################################
@@ -14,7 +17,16 @@ make_time(N, T) = range(0, T/N, N)
 brownian(N, T, start = 0.0) = make_walk(rand(Normal(0, sqrt(T/N)), N-1), start)
 
 # Helper to cover the normal distributions from a brownian motion
-get_steps(B) = [B[i] - B[i-1] for i in 2:length(B)]
+function get_steps(B, N)
+    step_size = Int(length(B)/N)
+    steps = [B[i] - B[i-step_size] for i in (step_size + 1):step_size:length(B)]
+end
+
+@testset "get_steps" begin
+    @test get_steps([0,2,4,6,8,10,12,14,16,18], 10) == fill(2, 9)
+    @test get_steps([0,2,4,6], 4) == [2,2,2]
+    @test get_steps([0,2,4,6,8,10,12,14,16,18], 5) == fill(4, 4)
+end
 
 geom(w, mu, sigma, t) = exp(sigma*w + (mu - sigma^2/2)*t)
 geometric(brownian, time, mu, sigma) = [geom(b,mu,sigma,t) for (b,t) in zip(brownian, time)]
@@ -25,10 +37,21 @@ geometric(brownian, time, mu, sigma) = [geom(b,mu,sigma,t) for (b,t) in zip(brow
 
 function eulers_method(B, N, T, a_fn, b_fn, start = 1.0)
     d = T/N
-    W = get_steps(B)
+    W = get_steps(B, N)
     fn(x,w,t) = x + a_fn(t, x)*d + b_fn(t, x)*w
     reduce((a,i) -> append!(a, fn(a[end], W[i], i)), [start], 1:length(W))
+    # extra = (length(B) - 1) % step_size
+    # append!(base, fill(base[end], extra))
 end
+
+@testset "eulers method" begin
+    fn(t,x) = x*t
+    @test eulers_method([0,1,2], 3, 1, fn, fn) == [1,3,15]
+    @test eulers_method([0,1,2,3], 4, 1, fn, fn, 2) == [2,6,30]
+    @test eulers_method([0,1,2,3], 4, 1, fn, fn) == [1,2.5,10]
+    @test eulers_method([0,1,2,3], 2, 2, fn, fn) == [1,1,4,4]
+end
+
 
 function geometric_euler(B, N, T, mu, sigma, start)
     a_fn(t, x) = mu*x
@@ -40,13 +63,17 @@ end
 # Simulations
 #######################################
 
-function plot_bs(N, T, mu = 2, sigma = 1.5, start = 1.0)
-    time = make_time(N, T)
-    B = brownian(N, T)
-    d = DataFrame(path = geometric(B, time, mu, sigma),
-                  euler = geometric_euler(B, N, T, mu, sigma, start),
-                  time = time)
-    plot(stack(d, [:euler, :path]), x = "time", y = "value", color = "variable", Geom.line)
+function plot_bs(N, T, mu = 2, sigma = 1.5, start = 1.0, B_N = 1000)
+    B = brownian(B_N, T)
+    path_time = make_time(B_N, T)
+    path = DataFrame(value = geometric(B, time, mu, sigma),
+                     time = time,
+                     variable = "Brownian Path")
+    euler_time = make_time(N, T)
+    euler = DataFrame(value = geometric_euler(B, N, T, mu, sigma, start),
+                      time = euler_time,
+                      variable = "Euler Approximation")
+    plot([path; euler], x = "time", y = "value", color = "variable", Geom.line)
 end
 
 function plot_vasicek(N, T, mu, sigma, a, start = 1.0)
@@ -100,14 +127,6 @@ plot_cir(1000, 10, .05, 4, 100)
 
 
 ##############################
-@testset "eulers method" begin
-    fn(t,x) = x*t
-    @test eulers_method([0,1,2], 1, 1, fn, fn) == [1,3,15]
-    @test eulers_method([0,1,2], 1, 1, fn, fn, 2) == [2,6,30]
-    @test eulers_method([0,1,2], 2, 1, fn, fn) == [1,2.5,10]
-    @test eulers_method([0,1,2], 4, 2, fn, fn) == [1,2.5,10]
-end
-
 
 
 
