b5f950d244d0ce1a49421ceba7b0e109d2dffa7d
Nandan Rao <nandanmarkrao@gmail.com>
2017-03-12 22:07:22 +0100

    weaved and finished plot

--- three/scratch.jl
+++ three/scratch.jl
@@ -20,31 +20,36 @@ geometric(brownian, time, mu, sigma) = [geom(b,mu,sigma,t) for (b,t) in zip(brow
 # Asians
 #######################################
 
-function expected_value(y, s, i, N, T, K)
+discount(r, N, i) = exp(-r * (N - i))
+
+function expected_value(y, s, r, i, N, T)
     # T/N is like 1/T in discrete
-    # time_left is like 1/T * (T - t)
-    time_left = T/N * (N - i)
-    mu = (y * T/N) + time_left * s - K
-    sigma = sqrt(time_left/3) # why 3 ??? Seems to work???
-    prob_in_the_money = 1 - cdf(Normal(mu, sigma), 0)
-    prob_in_the_money * mean(TruncatedNormal(mu, sigma, 0, Inf))
+    # N - i is like (T - t)
+    d = discount(r, N, i)
+    y * T/N * d + s/r * T/N * (1 - d)
 end
 
-function asian_value(S, K, N, T)
+function hedge(r, i, N, T)
+    T/N * r * (1 - discount(r, N, i))
+end
+
+function asian_value(S, r, N, T)
     Y = cumsum(S)
-    [expected_value(Y[i], S[i], i, N, T, K) for i in 1:N-1]
-end
-
-function make_path(r, mu, sigma, N, T)
-    time = make_time(N, T)
-    lambda = (mu - r)/sigma
-    W = brownian(N, T) - lambda*time # adjusting the prices, probably not what we want...
-    geometric(W, time, mu, sigma)
+    [expected_value(Y[i], S[i], r, i, N, T) for i in 1:N]
 end
 
 
 function path_and_price(r, mu, sigma, N, T)
+    time = make_time(N, T)
+    asset = geometric(brownian(N,T), time, mu, sigma)
+    value = asian_value(asset, r, N, T)
+    vcat(DataFrame(value = value, time = time, variable = fill("Asian Option", N)),
+         DataFrame(value = asset, time = time, variable = fill("Asset Price", N)))
+end
 
+function plot_asian(r, mu, sigma, N, T)
+    d = path_and_price(r, mu, sigma, N, T)
+    plot(d, x = :time, y = :value, color = :variable, Geom.line)
 end
 
 
